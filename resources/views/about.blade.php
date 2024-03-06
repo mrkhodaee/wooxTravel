6cea44ce7eac21f26a08d21f3a4fdbfbd0e51257
M.R Khodaee <mrkhodaee24@gmail.com>
2024-03-06 10:52:23 -0800

    some words translate about file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,13 +9,13 @@
         <div class="col-lg-12">
           <div class="content">
             <div class="blur-bg" style="background-image: url('{{ asset('assets/images/'.$country->image) }}');"></div>
-            <h4>EXPLORE OUR COUNTRY</h4>
+            <h4>کشور ما را کاوش کنید</h4>
             <div class="line-dec"></div>
             <h2>به {{ $country->name }} خوش آمدید</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uttersi labore
               et dolore magna aliqua is ipsum suspendisse ultrices gravida</p>
             <div class="main-button">
-              <a href="reservation.html">Discover More</a>
+              <a href="reservation.html">بیشتر</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <div class="section-heading text-center">
-            <h2>Best Weekly Offers In Each City</h2>
+            <h2>بهترین پیشنهادات هفتگی در هر شهر</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
           </div>
         </div>
@@ -76,10 +76,8 @@
                   <h6>{{ $city->price }}<br><span>/person</span></h6>
                   <div class="line-dec"></div>
                   <ul>
-                    <li>Deal Includes:</li>
-                    <li><i class="fa fa-taxi"></i>سفر {{ $city->num_days }} روزه > شامل هتل</li>
-                    <li><i class="fa fa-plane"></i> Airplane Bill Included</li>
-                    <li><i class="fa fa-building"></i> Daily Places Visit</li>
+                    <li>معامله شامل:</li>
+                    <li><i class="fa fa-taxi"></i> سفر {{ $city->num_days }} روزه > شامل هتل</li>
                   </ul>
                   <div class="main-button">
                     <a href="{{ route('traveling.reservation', $city->id) }}">رزرو</a>
@@ -104,32 +102,17 @@
         </div>
         <div class="col-lg-6">
           <div class="section-heading">
-            <h2>Discover More About Our Country</h2>
+            <h2>درباره کشور ما بیشتر بدانید</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
           </div>
           <div class="row">
-            <div class="col-lg-6">
-              <div class="info-item">
-                <h4>150.640 +</h4>
-                <span>Total Guests Yearly</span>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <div class="info-item">
-                <h4>175.000+</h4>
-                <span>Amazing Accomoditations</span>
-              </div>
-            </div>
+           
             <div class="col-lg-12">
               <div class="info-item">
                 <div class="row">
                   <div class="col-lg-6">
                     <h4>{{ count($cities) }}+</h4>
-                    <span>Amazing Places</span>
-                  </div>
-                  <div class="col-lg-6">
-                    <h4>240.580+</h4>
-                    <span>Different Check-ins Yearly</span>
+                    <span>شهر ها</span>
                   </div>
                 </div>
               </div>
@@ -138,7 +121,7 @@
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem
             ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
           <div class="main-button">
-            <a href="reservation.html">Discover More</a>
+            <a href="reservation.html">بیشتر</a>
           </div>
         </div>
       </div>
@@ -150,7 +133,7 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <div class="section-heading text-center">
-            <h2>Best Locations In Caribbeans</h2>
+            <h2>بهترین مکان ها در کارائیب</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
           </div>
         </div>
