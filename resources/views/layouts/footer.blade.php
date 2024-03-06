ab4437760701c9cc74b1f9af79f1f39e7b5b9071
M.R Khodaee <mrkhodaee24@gmail.com>
2024-03-06 10:42:21 -0800

    translate footer and reservation files

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Copyright © 2036 <a href="#">WoOx Travel</a> Company. All rights reserved.
-                    <br>Design: <a href="https://templatemo.com/" target="_blank"
-                        title="free CSS templates">TemplateMo</a> Distribution: <a
-                        href="https://themewagon.com target=/" _blank">ThemeWagon</a>
-                </p>
+                <p>.کپی رایت شرکت<a href="#"> ووکس تراول </a>تمامی حقوق محفوظ است</p>
             </div>
         </div>
     </div>
--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -5,10 +5,10 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-12">
-              <h4>Book Prefered Deal Here</h4>
-              <h2>Make Your Reservation</h2>
+              <h4>کتاب معامله ترجیحی در اینجا</h4>
+              <h2>رزرو خود را انجام دهید</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uttersi labore et dolore magna aliqua is ipsum suspendisse ultrices gravida</p>
-              <div class="main-button"><a href="about.html">Discover More</a></div>
+              <div class="main-button"><a href="about.html">بیشتر</a></div>
             </div>
           </div>
         </div>
@@ -20,22 +20,22 @@
             <div class="col-lg-4 col-sm-6">
               <div class="info-item">
                 <i class="fa fa-phone"></i>
-                <h4>Make a Phone Call</h4>
-                <a href="#">+123 456 789 (0)</a>
+                <h4>تماس تلفنی برقرار کنید</h4>
+                <a href="#">09919178860</a>
               </div>
             </div>
             <div class="col-lg-4 col-sm-6">
               <div class="info-item">
                 <i class="fa fa-envelope"></i>
-                <h4>Contact Us via Email</h4>
-                <a href="#">company@email.com</a>
+                <h4>از طریق ایمیل با ما تماس بگیرید</h4>
+                <a href="#">mrkhodaee24@gmail.com</a>
               </div>
             </div>
             <div class="col-lg-4 col-sm-6">
               <div class="info-item">
                 <i class="fa fa-map-marker"></i>
-                <h4>Visit Our Offices</h4>
-                <a href="#">24th Street North Avenue London, UK</a>
+                <h4>از دفاتر ما دیدن فرمایید</h4>
+                <a href="#">خیابان 24 خیابان شمالی لندن، انگلستان</a>
               </div>
             </div>
           </div>
@@ -55,25 +55,25 @@
                 @csrf
                 <div class="row">
                   <div class="col-lg-12">
-                    <h4>Make Your <em>Reservation</em> Through This <em>Form</em></h4>
+                    <h4><em>رزرو </em>خود را از طریق این<em> فرم </em>انجام دهید</h4>
                   </div>
                   <div class="col-lg-6">
                       <fieldset>
-                          <label for="Name" class="form-label">Your Name</label>
-                          <input type="text" name="name" class="Name" placeholder="Ex. John Smithee" autocomplete="on" required>
+                          <label for="Name" class="form-label">نام و نام خانوادگی</label>
+                          <input type="text" name="name" class="Name" placeholder="رضا :مثال" autocomplete="on" required>
                       </fieldset>
                   </div>
                   <div class="col-lg-6">
                     <fieldset>
-                        <label for="Number" class="form-label">Your Phone Number</label>
-                        <input type="text" name="phone_number" class="Number" placeholder="Ex. +xxx xxx xxx" autocomplete="on" required>
+                        <label for="Number" class="form-label">شماره موبایل</label>
+                        <input type="text" name="phone_number" class="Number" placeholder="09" autocomplete="on" required>
                     </fieldset>
                   </div>
                   <div class="col-lg-6">
                       <fieldset>
                           <label for="chooseGuests" class="form-label">Number Of Guests</label>
                           <select name="num_guest" class="form-select" aria-label="Default select example" id="chooseGuests" onChange="this.form.click()">
-                              <option selected>ex. 3 or 4 or 5</option>
+                              <option selected>انتخاب کنید</option>
                               <option type="checkbox" name="option1" value="1">1</option>
                               <option value="2">2</option>
                               <option value="3">3</option>
@@ -83,15 +83,13 @@
                   </div>
                   <div class="col-lg-6">
                     <fieldset>
-                        <label for="Number" class="form-label">Check In Date</label>
+                        <label for="Number" class="form-label">تاریخ ورود</label>
                         <input type="date" name="check_in_date" class="date" required>
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                       <fieldset>
-                          <label for="chooseDestination" class="form-label">Choose Your Destination</label>
                           <input type="hidden"  value="{{ $city->name }}" name="destination" class="Name" placeholder="Ex. John Smithee" autocomplete="on" required>
-                          
                       </fieldset>
                   </div>
                   {{-- <div class="col-lg-12">
@@ -104,7 +102,7 @@
                   
                   <div class="col-lg-12">                        
                       <fieldset>
-                          <button type="submit" class="main-button">Make Your Reservation Now</button>
+                          <button type="submit" class="main-button">ایجاد رزرو</button>
                       </fieldset>
                   </div>
                 </div>
